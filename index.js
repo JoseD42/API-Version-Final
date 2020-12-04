f4232729c8b4d76d1be72f084bc890f5fe08e69a
JoseD42 <josedanielbecerra42@gmail.com>
2020-12-03 20:27:20 -0700

    Upload Final

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import expressGraphql from 'express-graphql';
+import Schema from './schema/Schema.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import multer from 'multer';
+import path from 'path';
+import fileUpload from 'express-fileupload';
+
+const {graphqlHTTP} = expressGraphql;
+
+const port = 5000;
+
+
+const __dirname = path.resolve(path.dirname(''));
+
+var picSchema= new mongoose.Schema({
+    picpath:String
+})
+
+var picModel = mongoose.model('picsdemo',picSchema);
+
+var storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'./public/uploads')
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname)
+    }
+})
+var upload = multer({storage:storage})
+
+
+const app = express();
+
+app.use(cors());
+
+const dbName = "Games-API";
+const user = 'admingame';
+const password = 'contraeña2';
+const connectionString = `mongodb+srv://${user}:${password}@clustergame.7pbsg.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+.then(console.log('connected to games-db'))
+.catch(error => console.log(`[Error]: ${error}`));
+
+app.use('/graphql', graphqlHTTP({
+    schema: Schema,
+    graphiql: true
+}));
+
+// middle ware
+app.use(express.static('public'))
+app.use(cors())
+app.use(fileUpload());
+
+app.post('/upload', (req, res) => {
+
+    if (!req.files) {
+        return res.status(500).send({ msg: "file is not found" })
+    }
+
+    const myFile = req.files.file;
+
+    // Use the mv() method to place the file somewhere on your server
+    myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
+        if (err) {
+            console.log(err)
+            return res.status(500).send({ msg: "fe" });
+        }
+        return res.send({ file: myFile.name, path: `/${myFile.name}`, ty: myFile.type });
+    });
+})
+
+app.listen(port, console.log(`listening at: http://localhost:${port}/graphql`));
---
+++ schema/MutationType.js
@@ -0,0 +1,85 @@
+import graphql from 'graphql';
+import Game from '../models/Game.js';
+
+import GenreType from './GenreType.js';
+import GameType from './GameType.js';
+import Genre from '../models/Genre.js';
+
+
+const {GraphQLID, GraphQLString, GraphQLObjectType} = graphql;
+
+
+const MutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        //game
+        addGame: {
+            type: GameType,
+            args: {
+                name: {type: GraphQLString},
+                author: {type: GraphQLString},
+                image:{type: GraphQLString},
+                description: {type: GraphQLString},
+                fileP: {type: GraphQLString},
+                genreId: {type: GraphQLID}
+            },
+            resolve(parent, args){
+                let game = new Game(args);
+                return game.save();
+            }
+        },
+        editGame: {
+            type: GameType,
+            args:{
+                id: {type: GraphQLID},
+                name: {type: GraphQLString},
+                author:{type: GraphQLString},
+                image:{type: GraphQLString},
+                description:{type: GraphQLString},
+                fileP: {type: GraphQLString},
+                genreId: {type: GraphQLID}
+            },
+            resolve(parent, args){
+                return Game.findByIdAndUpdate(args.id, args);
+            }
+        },
+        deleteGame: {
+            type: GameType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                return Game.findByIdAndRemove(args.id);
+            }
+        },
+        //genre
+        addGenre: {
+            type: GenreType,
+            args: {
+                name: {type: GraphQLString}
+            },
+            resolve(parent, args){
+                const genre = new Genre(args);
+                return genre.save();
+            }
+        },
+        editGenre: {
+            type: GenreType,
+            args: {
+                id: {type: GraphQLID},
+                name: {type: GraphQLString}
+            },
+            resolve(parent, args){
+
+                return Genre.findByIdAndUpdate(args.id, args);
+            }
+        },
+        deleteGenre: {
+            type: GenreType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                return Genre.findByIdAndRemove(args.id);
+            }
+        },
+    }
+});
+
+export default MutationType;
